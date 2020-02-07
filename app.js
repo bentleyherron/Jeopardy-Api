08def834e581d0123cbca62415ec541b3f05fa25
Bentley Herron <10934614+bentleyherron@users.noreply.github.com>
2020-02-07 14:40:28 -0500

    Added documentation

--- app.js
+++ app.js
@@ -46,29 +46,29 @@ app.use('/api', apiRouter);
 
 /////////////////////////////////////////////////////////////
 //      CATCHALL
-app.use('*', (req, res) => {
-  res.status(404).send({
-    success: false,
-    time: new Date(),
-    message: "The specified resource could not be found.",
-  })
-})
+// app.use('*', (req, res) => {
+//   res.status(404).send({
+//     success: false,
+//     time: new Date(),
+//     message: "The specified resource could not be found.",
+//   })
+// })
 /////////////////////////////////////////////////////////////
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
