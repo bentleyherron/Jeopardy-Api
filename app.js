f6bf39e57e9bcc00a84adc15cc4366469c2640c6
Bentley Herron <10934614+bentleyherron@users.noreply.github.com>
2020-04-28 13:12:06 -0400

    Roll back change to path due to bug in certbot

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public', { dotfiles: 'allow' })));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Add helmet for headers
 const helmet = require('helmet');
